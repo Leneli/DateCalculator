24676f93d9af305c8980fe9c26e05afb25374284
Elena Kuklina <liafell.lansera@gmail.com>
2024-09-26 19:56:40 +0300

    Initial Commit

---
+++ DateCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DateCalculator
+//
+//  Created by Elena Kuklina on 26.09.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
